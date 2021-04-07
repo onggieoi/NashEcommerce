7d8dbf854c49171d1a96fa9893b4655bcb6a0c6d
quangduccao <onggieoi@gmail.com>
2021-04-07 07:11:36 +0700

    add to cart UI with Product ViewComponent

--- client/wwwroot/js/site.js
+++ client/wwwroot/js/site.js
@@ -3,21 +3,6 @@
     $(".addtocart").click(function (event) {
         event.preventDefault();
 
-        // $(this).siblings().each(function () {
-        //     if ($(this).hasClass("productId")) {
-        //         var productId = $(this).val();
-        //         console.log(productId);
-        //     }
-        //     if ($(this).hasClass("name")) {
-        //         var name = $(this).val();
-        //         console.log(name);
-        //     }
-        //     if ($(this).hasClass("price")) {
-        //         var price = $(this).val();
-        //         console.log(price);
-        //     }
-        // })
-
         var form = $(this).parent();
 
         var productId = form.find("input[name='productId']").val();
@@ -29,36 +14,28 @@
         var categoryName = form.find("input[name='categoryName']").val();
         var quantity = form.find("input[name='quantity']").val();
 
-        console.log(productId, quantity, name, price, image, description, rated, categoryName);
+        // console.log(productId, quantity, name, price, image, description, rated, categoryName);
 
-        $.post(`/order/${productId}`, { name, price, image, description, rated, categoryName, quantity }, function (e) {
-            if (e.length !== undefined && e.length > 0) {
-                console.log(e);
-                var total = 0;
-                var count = 0;
+        if (quantity > 0) {
+            $.post(`/order/${productId}`, { name, price, image, description, rated, categoryName, quantity }, function (e) {
+                if (e.length !== undefined && e.length > 0) {
+                    var total = 0;
+                    var count = 0;
 
-                $.each(e, function (i, val) {
-                    count += val.quantity;
-                    total += val.quantity * val.product.price;
-                });
+                    $.each(e, function (i, val) {
+                        count += val.quantity;
+                        total += val.quantity * val.product.price;
+                    });
 
-                console.log(total, count);
-
-                $("#cart_total").empty();
-
-                $("#cart_total").append(`<div>${count} <span>- ${total} $</span></div>`);
-            }
-        })
-
+                    $("#cart_total").empty();
+                    $("#cart_total").append(`<div>${count} <span>- ${total} $</span></div>`);
+                }
+            })
+        }
     });
-
-    console.log(buys);
 
     $("#buy").submit(function (event) {
         event.preventDefault();
-        console.log(event);
-        console.log($('#buy').serializeArray());
-
         var dataFrom = $('#buy').serializeArray();
 
         var productId = dataFrom.find(data => data.name === 'productId').value;
@@ -70,11 +47,10 @@
         var categoryName = dataFrom.find(data => data.name === 'categoryname').value;
         var quantity = dataFrom.find(data => data.name === 'quantity').value;
 
-        console.log(productId, quantity, name, price, image, description, rated, categoryName);
+        // console.log(productId, quantity, name, price, image, description, rated, categoryName);
 
         $.post(`/order/${productId}`, { name, price, image, description, rated, categoryName, quantity }, function (e) {
             if (e.length !== undefined && e.length > 0) {
-                console.log(e);
                 var total = 0;
                 var count = 0;
 
@@ -83,10 +59,7 @@
                     total += val.quantity * val.product.price;
                 });
 
-                console.log(total, count);
-
                 $("#cart_total").empty();
-
                 $("#cart_total").append(`<div>${count} <span>- ${total} $</span></div>`);
             }
         })
