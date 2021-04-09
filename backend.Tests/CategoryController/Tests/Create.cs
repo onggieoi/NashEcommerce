bbeb1ef1924bd0dfd281aab2d7f6d5e9bbe34fc1
quangduccao <onggieoi@gmail.com>
2021-04-09 10:43:01 +0700

    Add Unit Test Category Controller Create

---
+++ backend.Tests/CategoryController/Tests/Create.cs
@@ -0,0 +1,47 @@
+using System.Threading.Tasks;
+using backend.Controllers;
+using backend.Repositories.CategoryRepo;
+using Microsoft.AspNetCore.Mvc;
+using ViewModelShare.Category;
+using Xunit;
+
+namespace backend.Tests.CategoryController.Tests
+{
+    public class Create : IClassFixture<SqliteInMemoryFixture>
+    {
+        private readonly SqliteInMemoryFixture _fixture;
+
+        public Create(SqliteInMemoryFixture fixture)
+        {
+            _fixture = fixture;
+            _fixture.CreateDatabase();
+        }
+
+        [Fact]
+        public async Task Create_Success()
+        {
+            // Arrange
+            var loggerController = Loggers.CategoryController();
+
+            var mapper = Mapper.Get();
+
+            var dbContext = _fixture.Context;
+
+            var categoryRequest = NewDatas.NewCategoryRequest();
+
+            var categoryRepository = new CategoryRepository(mapper, dbContext);
+            var catgoriesController = new CategoriesController(loggerController, categoryRepository);
+
+            // Act
+            var result = await catgoriesController.Create(categoryRequest);
+
+            // Assert
+            var createdCategoryResult = Assert.IsType<CreatedResult>(result.Result);
+            var resultValue = Assert.IsType<CategoryRespone>(createdCategoryResult.Value);
+
+            Assert.Equal(categoryRequest.Name, resultValue.Name);
+            Assert.Equal(categoryRequest.Description, resultValue.Description);
+            Assert.Equal(categoryRequest.Image, resultValue.Image);
+        }
+    }
+}
