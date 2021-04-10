a2af3530ba4f4af150cf9baebd3d41e90dd6c12b
quangduccao <onggieoi@gmail.com>
2021-04-10 19:09:04 +0700

    Add oidc-client Service

---
+++ admin/src/services/auth-service.ts
@@ -0,0 +1,46 @@
+import { User, UserManager, UserManagerSettings } from "oidc-client";
+
+const oidcSettings: UserManagerSettings = {
+  authority: "https://localhost:5000",
+  client_id: "admin",
+  redirect_uri: "http://localhost:3000/authentication/login-callback",
+  post_logout_redirect_uri: "http://localhost:3000/authentication/logout-callback",
+  response_type: "code",
+  scope: "api openid profile",
+  automaticSilentRenew: true,
+  includeIdTokenInSilentRenew: true,
+};
+
+class AuthService {
+  public userManager: UserManager;
+
+  constructor() {
+    this.userManager = new UserManager(oidcSettings);
+  }
+
+  public getUserAsync(): Promise<User | null> {
+    return this.userManager.getUser();
+  }
+
+  public loginAsync(): Promise<void> {
+    return this.userManager.signinRedirect();
+  }
+
+  public completeLoginAsync(url: string): Promise<User> {
+    return this.userManager.signinCallback(url);
+  }
+
+  public renewTokenAsync(): Promise<User> {
+    return this.userManager.signinSilent();
+  }
+
+  public logoutAsync(): Promise<void> {
+    return this.userManager.signoutRedirect();
+  }
+
+  public async completeLogoutAsync(url: string): Promise<void> {
+    await this.userManager.signoutCallback(url);
+  }
+}
+
+export default new AuthService();
