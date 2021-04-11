950da0ddf7df638b7059cdf124452db76c6527dd
quangduccao <onggieoi@gmail.com>
2021-04-11 21:59:22 +0700

    Add Header

--- admin/src/containers/Layout.tsx
+++ admin/src/containers/Layout.tsx
@@ -1,43 +1,48 @@
 import React from 'react';
+import { Activity, Box, ChevronDown, Home } from 'react-feather';
 import { Link } from 'react-router-dom';
+import Header from 'src/components/Header';
 
 const Layout = ({ children }) => {
   return (
     <div className='flex'>
       <nav className="side-nav">
         <a href="" className="intro-x flex items-center pl-5 pt-4">
-          <img alt="Midone Tailwind HTML Admin Template" className="w-6" src="dist/images/logo.svg" />
+          <img alt="Admin" className="w-6" src="/images/logo.svg" />
           <span className="hidden xl:block text-white text-lg ml-3"> Mid<span className="font-medium">one</span> </span>
         </a>
         <div className="side-nav__devider my-6"></div>
         <ul>
           <li>
             <a href="index.html" className="side-menu side-menu--active">
-              <div className="side-menu__icon"> <i data-feather="home"></i> </div>
+              <div className="side-menu__icon"> <Home /> </div>
               <div className="side-menu__title"> Dashboard </div>
             </a>
           </li>
           <li>
-            <a href="javascript:;" className="side-menu">
-              <div className="side-menu__icon"> <i data-feather="box"></i> </div>
-              <div className="side-menu__title"> Menu Layout <i data-feather="chevron-down" className="side-menu__sub-icon"></i> </div>
+            <a href="" className="side-menu">
+              <div className="side-menu__icon"> <Box /> </div>
+              <div className="side-menu__title">
+                Menu Layout
+                <ChevronDown className="side-menu__sub-icon"></ChevronDown>
+              </div>
             </a>
             <ul className="">
               <li>
                 <a href="index.html" className="side-menu">
-                  <div className="side-menu__icon"> <i data-feather="activity"></i> </div>
+                  <div className="side-menu__icon"> <Activity /> </div>
                   <div className="side-menu__title"> Side Menu </div>
                 </a>
               </li>
               <li>
                 <a href="simple-menu-dashboard.html" className="side-menu">
-                  <div className="side-menu__icon"> <i data-feather="activity"></i> </div>
+                  <div className="side-menu__icon"> <Activity /> </div>
                   <div className="side-menu__title"> Simple Menu </div>
                 </a>
               </li>
               <li>
                 <a href="top-menu-dashboard.html" className="side-menu">
-                  <div className="side-menu__icon"> <i data-feather="activity"></i> </div>
+                  <div className="side-menu__icon"> <Activity /> </div>
                   <div className="side-menu__title"> Top Menu </div>
                 </a>
               </li>
@@ -48,6 +53,7 @@ const Layout = ({ children }) => {
       </nav>
 
       <div className='content'>
+        <Header />
         {children}
       </div>
     </div>
