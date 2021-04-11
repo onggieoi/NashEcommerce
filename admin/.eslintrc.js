83fbc6e4c610a7c2e363fab9a0246b39460b0a47
quangduccao <onggieoi@gmail.com>
2021-04-11 12:10:35 +0700

    eslint ignore empty function

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/ban-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": "off",
   }
 };
