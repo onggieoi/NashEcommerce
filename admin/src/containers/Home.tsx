8bcc1fab342ccc33810a9fe19c625ce182b08a0b
quangduccao <onggieoi@gmail.com>
2021-04-11 14:56:19 +0700

    rm cached index.css

--- admin/src/containers/Home.tsx
+++ admin/src/containers/Home.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from 'src/redux/ducks/counter';
+import { getTodos } from 'src/redux/ducks/todo';
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  const count = useSelector((state: any) => state.counter.count);
+  const todos = useSelector((state: any) => state.todo.todos);
+
+  console.log(todos);
+
+  const plusHandle = () => {
+    dispatch(increment());
+  };
+
+  const minusHandle = () => {
+    dispatch(decrement());
+  }
+
+  useEffect(() => {
+    dispatch(getTodos());
+  }, []);
+
   return (
-    <div>Home</div>
+    <div>
+      <div>Home</div>
+
+      <div className='flex'>
+        <button onClick={minusHandle}>Minus</button>
+        <div className='mx-5'>{count}</div>
+        <button onClick={plusHandle} >Plus</button>
+      </div>
+    </div>
   )
 };
 
