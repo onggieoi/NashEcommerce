3265ee8a206637c60fbfd1b6ea227d452153e7d9
quangduccao <onggieoi@gmail.com>
2021-04-11 17:57:40 +0700

    Fixed scss with postcss

--- admin/postcss.config.js
+++ admin/postcss.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")]
+/* eslint-disable @typescript-eslint/no-var-requires */
+module.exports =  {
+    syntax: 'postcss-scss',
+    plugins: [
+      require("postcss-import"),
+      require("tailwindcss"),
+      require("postcss-nested"),
+      require("autoprefixer"),
+    ],
 };
