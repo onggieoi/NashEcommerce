e5b2496e34284713145ed61ea162bb45a5600374
quangduccao <onggieoi@gmail.com>
2021-04-16 23:15:53 +0700

    Add IFormFile Http features

--- ViewModelShare/Category/CategoryRequest.cs
+++ ViewModelShare/Category/CategoryRequest.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http;
+
 namespace ViewModelShare.Category
 {
     public class CategoryRequest
@@ -5,5 +7,6 @@ namespace ViewModelShare.Category
         public string Name { get; set; }
         public string Image { get; set; }
         public string Description { get; set; }
+        public IFormFile ImageFile { get; set; }
     }
 }
