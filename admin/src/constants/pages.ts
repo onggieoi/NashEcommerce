bddc3dc47e19dc466f7159de473802cddc26df48
quangduccao <onggieoi@gmail.com>
2021-04-17 08:23:46 +0700

    add page endpoints

--- admin/src/constants/pages.ts
+++ admin/src/constants/pages.ts
@@ -1,5 +1,13 @@
 export const DASHBOARD = '/';
 export const AUTH = '/authentication/:action';
 export const LOGIN = '/authentication/login';
+
 export const CATEGORY = '/category';
+export const LIST_CATEGORY = '/category/';
+export const CREATE_CATEGORY = '/category/create';
+export const EDIT_CATEGORY = '/category/edit:id';
+
 export const PRODUCT = '/product';
+export const LIST_PRODUCT = '/product/';
+export const CREATE_PRODUCT = '/product/create';
+export const EDIT_PRODUCT = '/product/edit:id';
