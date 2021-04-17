df5d4f5a256a7146d1a9e9c53682400df3adba9b
quangduccao <onggieoi@gmail.com>
2021-04-17 16:43:06 +0700

    Add IFormFile ImageFile

--- ViewModelShare/Product/ProductRequest.cs
+++ ViewModelShare/Product/ProductRequest.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http;
+
 namespace ViewModelShare.Product
 {
     public class ProductRequest
@@ -5,6 +7,7 @@ namespace ViewModelShare.Product
         public string Name { get; set; }
         public int Price { get; set; }
         public string Image { get; set; }
+        public IFormFile ImageFile { get; set; }
         public string Description { get; set; }
         public int Rated { get; set; }
         public int CategoryId { get; set; }
