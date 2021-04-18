c245f2158092bcb6626cdaadd6c05ae13d1c02f1
quangduccao <onggieoi@gmail.com>
2021-04-18 19:52:51 +0700

    Add Customer Response

---
+++ ViewModelShare/Customer/CustomerResponse.cs
@@ -0,0 +1,11 @@
+
+namespace ViewModelShare.Customer
+{
+    public class CustomerResponse
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
