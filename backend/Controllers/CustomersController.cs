a141aa07cd5edbeb4e772ae193c53172f9969523
quangduccao <onggieoi@gmail.com>
2021-04-18 19:54:29 +0700

    Add Customer list feature

---
+++ backend/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using ViewModelShare.Product;
+using backend.Repositories.ProductRepo;
+using backend.Constans;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using backend.Configs;
+using backend.Repositories.CustomerRepo;
+using Microsoft.AspNetCore.Identity;
+using ViewModelShare.Customer;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerRepository customerRepository;
+        private ILogger<ProductsController> _logger;
+
+        public CustomersController(ICustomerRepository _customerRepository,
+            ILogger<ProductsController> logger)
+        {
+            customerRepository = _customerRepository;
+            _logger = logger;
+        }
+
+        // [Authorize("Admin")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CustomerResponse>>> GetProducts()
+        {
+            var customers = await customerRepository.GetCustomers();
+
+            return Ok(customers);
+        }
+    }
+}
