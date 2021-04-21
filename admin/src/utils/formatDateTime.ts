f29c835d21364aad86c0b33413d4cea356dbf6d0
quangduccao <onggieoi@gmail.com>
2021-04-21 11:48:51 +0700

    fixed format datetime

--- admin/src/utils/formatDateTime.ts
+++ admin/src/utils/formatDateTime.ts
@@ -1,1 +1,7 @@
-export default (date: string): string =>  new Date(date).toLocaleString();
+export default (date: string): string => new Date(date).toLocaleString('en',{
+    year: "numeric",
+    month: "2-digit",
+    day: "numeric",
+    hour: "2-digit",
+    timeZone: 'Asia/Ho_Chi_Minh',
+});
