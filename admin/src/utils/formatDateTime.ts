fffba4830b1be314f0cb5b6d208672ab699c9b2a
quangduccao <onggieoi@gmail.com>
2021-04-27 18:03:19 +0700

    format datetime

--- admin/src/utils/formatDateTime.ts
+++ admin/src/utils/formatDateTime.ts
@@ -1,7 +1,14 @@
-export default (date: string): string => new Date(date).toLocaleString('en',{
-    year: "numeric",
-    month: "2-digit",
-    day: "numeric",
-    hour: "2-digit",
-    timeZone: 'Asia/Ho_Chi_Minh',
-});
+export default (date: string): string => 
+{
+    const dateUtc = new Date(date).toUTCString();
+    
+    // return new Date(dateUtc).toLocaleString('en-US',{
+    //     year: "numeric",
+    //     month: "2-digit",
+    //     day: "numeric",
+    //     hour: "2-digit",
+    //     // timeZone: 'Asia/Ho_Chi_Minh',
+    // });
+
+    return dateUtc.split(',')[1].replace('GMT', '');
+}
